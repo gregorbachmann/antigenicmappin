b879e4d536565abd4bb3a02c60404a59fcf2e08b
gregorb <gregorb@student.ethz.ch>
2018-12-18 10:50:23 +0100

    Updated README

--- EmbeddingAlgos.R
+++ EmbeddingAlgos.R
@@ -6,16 +6,16 @@
 isomapvis <- function(data, dim, nn, plot=TRUE,label=FALSE){
   ##############################################################################
   # Args:
-  #         data:   A list containing the symmetric matrix with NA's and the 
-  #                 names of viruses and antibodies (Result of the function ReadIn for instance)  
+  #         data:   A list containing the symmetric matrix with NA's and the
+  #                 names of viruses and antibodies (Result of the function ReadIn for instance)
   #          dim:    Desired dimension of the embedding, if 2 or 3 a plot can be made
   #           nn:     Number of Nearest Neighbours used in Isomap
   #         plot:   Either TRUE or FALSE
   #        label:  Either TRUE or FALSE
-  # Returns: 
+  # Returns:
   #         Embedding found via Isomap
   ################################################################################
-  
+
   library('vegan')
   library('plot3D')
   library("plot3Drgl")
@@ -27,7 +27,7 @@ isomapvis <- function(data, dim, nn, plot=TRUE,label=FALSE){
     emb <- as.data.frame(embedding$points)
     colnames(emb) <- c("Axis1","Axis2")
     p <- ggplot(emb, aes(x=Axis1, y=Axis2,label=data$names,shape=Label,col=IsolationWeek)) + geom_point(size=2)+ggtitle("Isomap Embedding")+
-         theme(plot.title = element_text(hjust = 0.5)) 
+         theme(plot.title = element_text(hjust = 0.5))
     if (label == TRUE){
       p <- p + geom_label(vjust = 0) + geom_point(size=2)
     }
@@ -54,44 +54,44 @@ mdsvis <- function(data, dim, method, init="random", plot=TRUE,label=TRUE,real){
   ################################################################################
   # Args:
   #     data:   A list containing the symmetric matrix with NA's and the names of viruses and antibodies
-  #             (Result of the function ReadIn for instance)        
+  #             (Result of the function ReadIn for instance)
   #      dim:    Desired dimension of the embedding, if 2 or 3 a plot is made
-  #   method: Either "interval", "ratio" or "ordinal"
+  #   method: Either "interval", "ratio", "ordinal" or "mspline"
   #     init:   Starting configuration
   #     plot:   Either TRUE or FALSE
   #     label:  Either TRUE or FALSE
   #     real:   Either TRUE or FALSE, indicating whether data has names and time attribut
-  # Returns: 
+  # Returns:
   #     Embedding found via SmaCof
   ###############################################################################
-  
+
   library('smacof')
   library('plot3D')
   library("plot3Drgl")
-  
+
   n <- length(data$symmetric[1,])
   # Need to specifiy a weight matrix to ignore the unknown distances
   weights <- matrix(1,n,n)
   for (i in 1:n){
     for (j in 1:n){
       if (is.na(data$symmetric[i,j])){
-      #if( data$symmetric[i,j]==100){ 
+      #if( data$symmetric[i,j]==100){
         weights[i,j] <- 0
       }
       else weights[i,j] <- 1
     }
   }
-  
+
   embedding <- smacofSym(data$symmetric,dim,method,weightmat=weights,init=init)
   Label <- as.factor(c(rep("Antibody",length(data$standard[,1])),rep("Strain",length(data$standard[1,]))))
   IsolationWeek <- as.factor(data$time)
-  
+
   # 2d version
   if (dim == 2 && real==TRUE){
     emb <- as.data.frame(embedding$conf)
     colnames(emb) <- c("Axis1","Axis2")
     p <- ggplot(emb, aes(x=Axis1, y=Axis2,label=data$names,shape=Label,col=IsolationWeek)) + geom_point(size=2)+ggtitle(paste(method,"Embedding"))+
-      theme(plot.title = element_text(hjust = 0.5)) 
+      theme(plot.title = element_text(hjust = 0.5))
     if (label == TRUE){
       p <- p + geom_label(vjust = 0) + geom_point(size=2)
     }
@@ -103,15 +103,15 @@ mdsvis <- function(data, dim, method, init="random", plot=TRUE,label=TRUE,real){
     emb <- as.data.frame(embedding$conf)
     colnames(emb) <- c("Axis1","Axis2")
     p <- ggplot(emb, aes(x=Axis1, y=Axis2,shape=Label)) + geom_point(size=2)+ggtitle(paste(method,"Embedding"))+
-      theme(plot.title = element_text(hjust = 0.5)) 
+      theme(plot.title = element_text(hjust = 0.5))
   }
-  
+
   # 3d version
   if (dim == 3 & plot == TRUE){
     scatter3D(embedding$conf[,1],embedding$conf[,2],embedding$conf[,3],labels=data$names,colvar=1:length(data$symmetric[,1]),col=data$col)
     if (label == TRUE){
       text3D(embedding$conf[,1],embedding$conf[,2],embedding$conf[,3],labels=data$names,add=TRUE)
-      
+
     }
     plotrgl()
   }
@@ -125,20 +125,20 @@ mdsvis <- function(data, dim, method, init="random", plot=TRUE,label=TRUE,real){
 
 sammon_mapping <- function(data,dim,plot=TRUE,label=FALSE,n.v,n.a){
   # Args:
-  #    data: List containing the symmetric and standard distance matrix and labels 
+  #    data: List containing the symmetric and standard distance matrix and labels
   #     dim: Dimension you want to project down
   #    plot: Boolean indicating whether plot should be produced
   #   label: Boolean indicating whether labels should be added to plot
   #     n.v: Number of viruses in data
   #     n.a: Number of antibodies
   # Returns:
-  #     Sammon Embedding 
+  #     Sammon Embedding
   ######################################################################################
-  
+
   numruns <- 10
   best_stress <- Inf
   best_embedding <- 0
-  
+
   # As algorithm gets stuck in local minimas very often, iterate 10 times:
   for (run in 1:numruns){
     # Get starting configuration (cmdscale doesn't accept NA's)
@@ -151,16 +151,16 @@ sammon_mapping <- function(data,dim,plot=TRUE,label=FALSE,n.v,n.a){
       best_stress <- embedding$stress
     }
   }
- 
+
    # 2D version
   if (dim==2 && plot==TRUE){
     plot(best_embedding$points)
-    
+
     if (label == TRUE){
       text2D(best_embedding$points[,1],best_embedding$points[,2],labels=data$names,add=TRUE)
     }
   }
-  
+
   # 3D version
   if (dim == 3 & plot == TRUE){
     scatter3D(best_embedding$points[,1],best_embedding$points[,2],best_embedding$points[,3])
@@ -177,13 +177,13 @@ cluster <- function(embedding,k,plot=TRUE,method){
   # Args:
   #     embedding: 2d coordinates for embedding
   #             k: number of clusters
-  #        method: Method to cluster 
+  #        method: Method to cluster
   #                (one of c("mspline","ordinal","interval","ratio","Isomap","Sammon"))
-  # Returns: 
+  # Returns:
   #     kmeans object
   #     Plot with clusters visualized
   ########################################################################
-  
+
   if (is.element(method,c("mspline","ordinal","interval","ratio"))){
     # Use kmeans with 10 initializations to avoid bad clusters
     clus <- kmeans(embedding$conf,k,nstart=10)
--- README.md
+++ README.md
@@ -1,16 +1,63 @@
-# AntigenicMapping
+# Antigenic Mapping
 
 Repository storing the code used in my project on Antigenic Mappings.
 
 ## Requirements and Packages
-The code is implemented in R using RStudio Version 1.1.456
+The code is implemented in R using **RStudio Version 1.1.456**
 The following R-packages are needed in order to run the code:
-* ggplot
-* gridExtra
-* phytools
-* vegan
-* plot3D and plot3plot3Drgl
-* smacof
-* MASS
-* clusterCrit
-* reshape
+* **ggplot**
+* **gridExtra**
+* **phytools**
+* **vegan**
+* **plot3D** and **plot3plot3Drgl**
+* **smacof**
+* **MASS**
+* **clusterCrit**
+* **reshape**
+
+## Structure of Code
+
+The main structure of my code consists of files that contain functions and algorithms which get called in the script files.
+A quick overview over the files:
+* **EmbeddingAlgos**: Contains the multidimensional scaling algorithms
+* **HelpFunctions**: Contains smaller functions that are needed to process data
+* **Read_In_File**: Function that reads a .csv file and converts the data as needed.
+* **ClusterCreator**: Contains all cluster-generating functions needed for the evaluation.
+* **EvaluateAlgos**: Contains the functions for the frameworks discussed in my work.
+* **EvaluationScript**: Evaluates all algorithms using the frameworks
+* **PlottingScript** Produces embeddings for the HIV data.
+
+## Form of data needed
+Assume we have IC-50 distances between N viral strains and M antibodies.
+I mainly worked with data in form of a list, containing the following:
+* **Standard distance matrix**: A matrix of size MxN containing the pairwise distance between antibodies and viral strains
+* **Symmetrized distance matrix**: The symmetrized version of size (M+N)x(M+N), with zeros on the diagonal and NAs for strain and antibody pairs.
+* **Isolation time**: Vector of size M+N, containing isolation time measured in weeks for each antibody and strain (optional)
+* **Labels**: Names of strains and antibodies (optional)
+### EmbeddingAlgos
+#### isomapvis
+Relies heavily on the function **isomap** from **vegan**.
+
+**Arguments**:
+* **data**: A list in the explained format.
+* **dim**: The desired embedding dimension
+* **n**: Number of nearest neighbours to consider for graph
+* **plot**: Boolean, the plot is displayed if TRUE
+* **label**: Boolean, labels are displayed in the plot if TRUE
+
+**Returns**:
+* Embedding returned by isomap
+* Plot object
+
+This function calculates the embedding by filling out the incomplete matrix using the neighbourhood graph. Note that **nn** needs to be chosen high enough in order to avoid fragmentation of the data.
+
+####mdsvis
+Relies heavily on the function **smacofSym** from the package **smacof**.
+
+**Arguments**:
+* **data**: A list in the explained format.
+* **dim**: The desired embedding dimension
+* **method**: One of "ratio", "interval", "mspline" or "ordinal" as discussed in the project.
+* **init**: "torgerson" for a deterministic initial configuration or "random" for a random one.
+* **plot**: Boolean, the plot is displayed if TRUE
+* **label**: Boolean, labels are displayed in the plot if TRUE
